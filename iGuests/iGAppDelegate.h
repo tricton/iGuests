ea1e4e565b3f807b2e7bb4fc188b4fdf5bf1e4ff
admin <admin@localhost.(none)>
2013-01-10 15:27:15 +0200

    Initial Commit

---
+++ iGuests/iGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iGAppDelegate.h
+//  iGuests
+//
+//  Created by admin on 10.01.13.
+//  Copyright (c) 2013 tricton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iGuests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iGuests
+//
+//  Created by admin on 10.01.13.
+//  Copyright (c) 2013 tricton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iGAppDelegate class]));
+    }
+}
